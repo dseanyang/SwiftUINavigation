b9c0e5a2db4aaf7b5d06355fec3832f4b3e1a733
dseanyang <dsean120577@gmail.com>
2020-07-09 14:20:01 +0800

    first commit

---
+++ SwiftUINavigation/SwiftUINavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINavigation
+//
+//  Created by 楊德忻 on 2020/7/9.
+//  Copyright © 2020 sean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
